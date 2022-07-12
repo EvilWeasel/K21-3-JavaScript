8a7a19a404f30d6dc2568e79069b16d3db3aef38
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-07-12 12:16:35 +0200

    quicker sync zur middagsbause

---
+++ Getting-Started/rechner.js
@@ -0,0 +1,7 @@
+let inputNum1 = document.getElementById("num1Input")
+let inputNum2 = document.getElementById("num2Input")
+let btn_calc = document.getElementById("btn_calc")
+let resultElement = document.getElementById("result")
+
+
+//getElementById('result').innerHTML = getElementById('num1Input').value + getElementById('num2Input').value
--- Getting-Started/tutorial.js
+++ Getting-Started/tutorial.js
@@ -33,3 +33,64 @@ let num2 = 22.456
 let massiveNumber = BigInt(Number.MAX_SAFE_INTEGER)
 console.log(massiveNumber)
 
+// Strings
+// type inference
+let string1 = "ABCDEF"
+// console.log(string1)
+// string1 = 1
+// console.log(string1)
+console.log(string1[0])
+string1 = string1 + "GHIJK"
+console.log(string1)
+// both examples below are strings, no chars like in C#
+let char = 'A'
+let char2 = "A"
+
+
+// Boolean
+let bool1 = true
+// when bool1 is false, it returns true, else it returns false
+bool1 = false ? 1 : "false"
+console.log(bool1)
+
+
+/* Error-Types / Eigene Definition */
+// Null
+// c#-context: public string? name => nullable
+// Variable "abc" has no pointer to a value in memory
+const abc = null
+console.log(abc)
+console.log(typeof(abc))
+
+// Undefined
+// example: declaring a variable and accessing it, before initialization
+let data
+console.log(data)
+// data.execCommand()
+
+// NaN => Not a Number
+console.log(undefined + 10)
+let userInputNumber
+console.log(userInputNumber * 20)
+
+/* Objects */
+let person1 = {
+    name: "Herbert",
+    nachname: "Mueller",
+    alter: 42, 
+    verheiratet: true, 
+    children: [
+        {
+            name: "Alf",
+            alter: 12
+        },
+        {
+            name: "Steffi",
+            alter: 6
+        }
+    ]
+}
+console.log(person1)
+console.log(person1.children.length)
+console.log(person1.children[0].name)
+// console.log(person1.children[2].alter)
